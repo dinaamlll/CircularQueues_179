383985c4e7a5b0b1fa120fa12b9b626872333fc8
dinaamlll <dinamaliaa17@gmail.com>
2023-06-23 11:08:18 +0700

    Iterasi dari aawal array hingga REAR

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -89,6 +89,12 @@ public:
 
 			FRONT_position = 0;
 
+			//Iterasi dari aawal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
 
 		}
 	}
