5c6537503bc6c2c03f526e8723acfba18be1d22e
dinaamlll <dinamaliaa17@gmail.com>
2023-06-23 09:35:42 +0700

    Jika REAR berada di posisi terakhir array, kembali ke awal array

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -22,6 +22,19 @@ public:
 			cout << "\nQueue overflow\n";
 			return;
 		}
+
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//Jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
-
